f7f4b1ddd4239475d0cf07ed5c7bf96ad46848b4
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 16:15:48 -0400

    10 programa kotlin | Controle de Fluxo com IF ELSE IF

---
+++ CursoKotlinUdemy/src/main/kotlin/decimoprograma/ExpressaoIFELSEIF.kt
@@ -0,0 +1,37 @@
+/**
+ * Expressão IF ELSE IF
+ */
+fun main(){
+
+    var valorA = 10
+    var valorB = 9
+
+    var i = valorA
+
+    if ((valorA == 15) || (valorB<=10)){
+
+        // Bloco de código  A
+        print("Validação A verdadeira")
+
+    } else if (valorB == 20){
+
+        // Bloco de código B
+        print("Validação B verdadeira")
+
+    } else if(i == null){
+
+        // Bloco de código C
+        print("Validação C verdadeira")
+
+    } else {
+
+        // Bloco de código D
+        print("Todas as Validações são falsas.")
+    }
+
+
+
+
+
+}
+
