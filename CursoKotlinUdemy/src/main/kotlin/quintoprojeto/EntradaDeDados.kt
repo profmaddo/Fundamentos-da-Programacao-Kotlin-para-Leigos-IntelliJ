b15c3c1738d652aa0b949ebdfe40d253a3b12a1c
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 15:47:38 -0400

    5º programa em Kotlin | Entrada de Dados via Teclado

---
+++ CursoKotlinUdemy/src/main/kotlin/quintoprojeto/EntradaDeDados.kt
@@ -0,0 +1,24 @@
+/**
+ * Comentário com mais detalhes
+ */
+fun main() {
+    // Meu primeiro programa
+
+    println("1º Programa em Kotlin...")
+    println("Qual o seu nome? ")
+
+    var nome:String = readLine()!!.toString()
+
+    println("Olá, $nome. Muito prazer...")
+
+    println("Qual sua idade? ")
+
+    var idade:Int = readLine()!!.toInt()
+
+    print("Sua idade é $idade")
+
+
+}
+
+
+
