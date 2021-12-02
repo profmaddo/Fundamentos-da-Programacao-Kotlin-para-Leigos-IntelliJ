f85ddcd8719937aff6707d28d2140f7c8e47ed0c
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 16:03:06 -0400

    9º programa kotlin | Controle de Fluxo com IF completo

--- CursoKotlinUdemy/src/main/kotlin/novoprograma/ExpressaoIF.kt
+++ CursoKotlinUdemy/src/main/kotlin/novoprograma/ExpressaoIF.kt
@@ -6,16 +6,28 @@ fun main(){
     var valorA = 10
     var valorB = 20
 
-    var i = null
+    var i = 23
 
     // Dicas para escrever IF
-    
+
+    if ((i == null) || (valorA <=8)) {
+
+        processar()
+    }
+
+    if ((i != null) && (valorB > valorA)) {
+
+        naoProcessar()
+    }
+
+    print("\nFim...")
+
 }
 
 fun processar(){
-    print("TRUE")
+    print("\n\nTRUE")
 }
 
 fun naoProcessar(){
-    print("FALSE")
+    print("\n\nFALSE")
 }
