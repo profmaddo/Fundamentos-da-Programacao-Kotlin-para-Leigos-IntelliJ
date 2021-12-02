6b85a848782e0d1e2767e03b6bd00a3502d14292
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 16:17:05 -0400

    11 programa kotlin | Controle de Fluxo WHEN

---
+++ CursoKotlinUdemy/src/main/kotlin/decimoprimeiroprograma/ExpressaoWhen.kt
@@ -0,0 +1,18 @@
+/**
+ * Expressão When
+ */
+fun main(){
+
+    var valorA = 2
+
+    when(valorA){
+
+        10 -> println("ValorA = 10")
+        20 -> println("ValorA = 20")
+        else -> {
+            println("valorA não é 10 nem é 20")
+        }
+
+    }
+
+}
