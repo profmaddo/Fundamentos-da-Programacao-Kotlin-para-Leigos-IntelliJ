08cc7d2eb1b9d02f1e8e557267bb24aefc3c399d
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 15:39:00 -0400

    3º programa em Kotlin | Calcular Anos de Vida

---
+++ CursoKotlinUdemy/src/main/kotlin/terceiroprojeto/CalcularAnosDeVida.kt
@@ -0,0 +1,24 @@
+/**
+ * Este programa responderá a pergunta abaixo:
+ *
+ * Como calcular quantos anos de vida uma pessoa tem?
+ */
+fun main(){
+
+    // CamelCase Java Python Kotlin
+    // Entrada de Dados
+    var anoDeNascimento = 2000
+    var anoAtual = 2021
+
+    // Processamento
+    var anosDeVida = anoAtual - anoDeNascimento
+
+    // Saída / Resultados
+    print("Como calcular quantos anos de vida uma pessoa tem?\n\n")
+    print("Ano de Nascimento: $anoDeNascimento\n")
+    print("Ano Atual........: $anoAtual\n\n")
+    print("Resultado........: $anosDeVida\n\n")
+    print("Você tem $anosDeVida anos de vida")
+
+
+}
