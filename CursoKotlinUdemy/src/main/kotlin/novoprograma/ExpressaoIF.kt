095849d56d59967991b3b1a843316ab0acd74c47
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 16:01:55 -0400

    9º programa kotlin | Controle de Fluxo com IF

---
+++ CursoKotlinUdemy/src/main/kotlin/novoprograma/ExpressaoIF.kt
@@ -0,0 +1,21 @@
+/**
+ * Expressão IF
+ */
+fun main(){
+
+    var valorA = 10
+    var valorB = 20
+
+    var i = null
+
+    // Dicas para escrever IF
+    
+}
+
+fun processar(){
+    print("TRUE")
+}
+
+fun naoProcessar(){
+    print("FALSE")
+}
