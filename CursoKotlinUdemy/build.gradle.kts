54848dd9813a33368ebd9dede6bc41ad825c5703
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 15:27:35 -0400

    1º Versionamento

---
+++ CursoKotlinUdemy/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+    application
+}
+
+group = "me.maddo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
