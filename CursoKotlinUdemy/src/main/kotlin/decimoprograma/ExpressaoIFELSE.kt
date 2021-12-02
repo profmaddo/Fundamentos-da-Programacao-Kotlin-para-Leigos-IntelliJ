979983c4f75a67e637a6bea19cf0e1cac22cad2c
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 16:05:04 -0400

    910 programa kotlin | Controle de Fluxo com IF ELSE

---
+++ CursoKotlinUdemy/src/main/kotlin/decimoprograma/ExpressaoIFELSE.kt
@@ -0,0 +1,32 @@
+/**
+ * Expressão IF ELSE
+ */
+fun main(){
+
+    var valorA = 10
+    var valorB = 20
+
+    var i = null
+
+    if (i == null) {
+
+        executarProcessamento()
+
+    } else {
+
+        naoExecutarProcessamento()
+    }
+
+    print("\n\nFIM...")
+
+}
+
+fun executarProcessamento(){
+
+    print("Processando...")
+}
+
+fun naoExecutarProcessamento(){
+
+    print("Sem processamento...")
+}
