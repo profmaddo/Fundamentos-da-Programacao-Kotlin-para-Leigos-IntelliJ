dcd52993851438241c753423ab0784475a59d6a2
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 15:44:02 -0400

    4º programa em Kotlin | Calcular Raio do Círculo

---
+++ CursoKotlinUdemy/src/main/kotlin/quartoprojeto/CalcularRaioCirculo.kt
@@ -0,0 +1,13 @@
+const val PI = 3.141516
+
+fun main(){
+
+    var circulo = 15
+//    val pi = 3.141516
+
+    var valorDoRaio = circulo / (2*PI)
+
+    print("Círculo mede: $circulo\n")
+    print("Valor do Raio do Círculo: $valorDoRaio\n")
+
+}
