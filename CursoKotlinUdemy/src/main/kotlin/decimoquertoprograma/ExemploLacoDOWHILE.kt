6c3124856509d637f9106920c8afa27942c27037
Profº Marco Maddo <profmaddo@gmail.com>
2021-12-02 16:30:54 -0400

    14º programa kotlin | Laço de Repetição - DO WHILE

---
+++ CursoKotlinUdemy/src/main/kotlin/decimoquertoprograma/ExemploLacoDOWHILE.kt
@@ -0,0 +1,18 @@
+/**
+ * Exemplo com Laço de Repetição
+ * do .... while
+ */
+fun main(){
+
+  var i = -3
+
+    do {
+
+        println("valor do i no laço vale: $i")
+
+        i = i - 2
+
+    } while (i >= 1)
+
+
+}
